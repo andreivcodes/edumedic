3dfaa830890faa97f1155700e3743fcc1aabe5af
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-02-24 23:38:30 +0200

    turso seed

--- src/server/prisma/seed.ts
+++ src/server/prisma/seed.ts
@@ -1,6 +1,15 @@
+import { createClient } from '@libsql/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
   const alice = await prisma.user.upsert({
